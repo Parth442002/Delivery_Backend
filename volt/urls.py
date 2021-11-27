db309a9cc05e7f917ecca115a4a30757209ec6a0
Parth442002 <parthkatiyar04@gmail.com>
2021-11-27 23:36:37 +0530

    added slash in urls

--- volt/urls.py
+++ volt/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/auth', include('dj_rest_auth.urls')),
+    path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls'))
